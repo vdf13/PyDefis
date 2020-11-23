511f131ba9fec3f9faa906c678b65c29371f6dd3
vdf13 <vdf13@free.fr>
2020-11-23 10:36:36 +0100

    Modification du programme en ajoutant une classe à chaque défi

--- pydefis.py
+++ pydefis.py
@@ -3,43 +3,80 @@
 # Code python pour les défis python du site https://pydefis.callicode.fr
 
 
-class PyDefisSW:
-    def SW4_Vitesse(x, y, z):
+class SW4Vitesse:
+    ''' Classe définissant l'objet défis SW4 Vitesse Lumière
+    - x -y -z sont les coordonnées en entrée
+    - fonction qui calcule les nouvelle coordonnées
+    '''
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def Vitesse(self):
         vitesse = []
-        while 10 * x > y:
-            x = (y * z) % 10000
-            y = (3 * z) % 10000
-            z = (7 * z) % 10000
-        vitesse = [x, y, z]
-        print("Coordonnées de passage en vitesse lumière", vitesse)
+        while 10 * self.x > self.y:
+            self.x = (self.y * self.z) % 10000
+            self.y = (3 * self.z) % 10000
+            self.z = (7 * self.z) % 10000
+        vitesse = [self.x, self.y, self.z]
+        print("SW4 Vitesse : Coordonnées de passage ", vitesse)
         return vitesse
 
-    def SW4_LunetteAstro(x, y, i):
+
+class SW4LunetteAstro:
+    ''' Classe définissant l'objet pydéfis SW4 Lunette Astro
+    - x -y coordonnées entrée, i nombre d'itération de la fonction '''
+
+    def __init__(self, x, y, i):
+        self.x = x
+        self.y = y
+        self.i = i
+
+    def Calcul(self):
         astro = []
-        for occ in range(i):
-            x1 = (x + 2 * y) % 2018
-            y1 = (-3 * x + y) % 2018
-            x = x1
-            y = y1
-        dec = (x - 900) / 10
-        asc = (y / 150) * 2
+        for occ in range(self.i):
+            x1 = (self.x + 2 * self.y) % 2018
+            y1 = (-3 * self.x + self.y) % 2018
+            self.x = x1
+            self.y = y1
+        dec = (self.x - 900) / 10
+        asc = (self.y / 150) * 2
         astro = [dec, asc]
-        print("Déclinaison, ascension :", astro)
+        print("SW4 Lunette Astro : Déclinaison, ascension :", astro)
         return astro
 
-    def SW7_EwokName(names):
+
+class SW7EwokName1:
+    ''' Classe définissant l'objet du pydéfis SW7 Ewok Name
+    - liste de noms
+    - Fonction qui cherche combien on un 'a' ou 'A' '''
+
+    def __init__(self, names):
+        self.names = names
+
+    def NombreA(self):
         count = 0
-        for name in names:
+        for name in self.names:
             if "a" in name.lower():
                 pass
             else:
                 count += 1
-        print("Nombre de Ewok sans lettre 'a' ou 'A' :", count)
+        print("SW7 Ewok Name: Nombre de Ewok sans lettre 'a', 'A' :", count)
         return(count)
 
-    def SW7_EwokName2(names):
+
+class SW7EwokName2:
+    ''' Classe définissant l'objet du pydéfis Ewok Name 2
+    - liste de noms en entrée
+    - Fonction qui compte combien on 2 *+ de consonnes que voyelles '''
+
+    def __init__(self, names):
+        self.names = names
+
+    def Double_Voyelle(self):
         count = 0
-        for name in names:
+        for name in self.names:
             # compter les voyelles
             voyelle = 0
             for lettre in name.lower():
@@ -53,24 +90,28 @@ class PyDefisSW:
             if ((len(name) - 1) - voyelle) / voyelle == 2:
                 count += 1
         print("Nombre de Ewoks avec \
-                le double de consonnes dans le nom :", count)
+le double de consonnes dans le nom :", count)
         return count
 
 
 # SW4 Vitesse lumière
-PyDefisSW.SW4_Vitesse(997, 312, 663)
+resultat = SW4Vitesse(997, 312, 663)
+print(resultat.Vitesse())
 
 # SW4 Lunette Astro
-PyDefisSW.SW4_LunetteAstro(1694, 1546, 50)
+resultat = SW4LunetteAstro(1694, 1546, 50)
+print(resultat.Calcul())
 
-# SW7 Ewok names
+# SW7 Ewok names I
 file_to_open = "fichiers_source//ewok_names.txt"
 with open(file_to_open, "r") as file:
     names = file.readlines()
-PyDefisSW.SW7_EwokName(names)
+resultat = SW7EwokName1(names)
+print(resultat.NombreA())
 
 # SW7 Ewok names 2
 file_to_open = "fichiers_source//ewok_names2.txt"
 with open(file_to_open, "r") as file:
     names = file.readlines()
-PyDefisSW.SW7_EwokName2(names)
+resultat = SW7EwokName2(names)
+print(resultat.Double_Voyelle())
--- test_pydefis.py
+++ test_pydefis.py
@@ -12,23 +12,29 @@ class TestPyDefisSW:
         x = 997
         y = 312
         z = 663
-        assert [501, 9461, 5409] == script.PyDefisSW.SW4_Vitesse(x, y, z)
+        # On crée une instance de l'objet avec les valeurs a tester
+        resultat_vitesse = script.SW4Vitesse(x, y, z)
+        assert [501, 9461, 5409] == resultat_vitesse.Vitesse()
 
     # SW IV : LunetteAstro . Il a mit son mot de passe sur un post-it
     def test_SW4_LunetteAstro(self):
         x = 1694
         y = 1546
         i = 50
-        assert [44, 4] == script.PyDefisSW.SW4_LunetteAstro(x, y, i)
+        # On crée une instance de l'objet avec les valeurs a tester
+        resultat_lunette = script.SW4LunetteAstro(x, y, i)
+        assert [44, 4] == resultat_lunette.Calcul()
 
     # SW VII : le nom de Ewoks !
     # Combien ne contiennent pas de a ou A ?
     def test_SW7_EwokName(self):
         names = ["Albert", "Barnabé", "Chris"]
-        assert 1 == script.PyDefisSW.SW7_EwokName(names)
+        resultat_ewok1 = script.SW7EwokName1(names)
+        assert 1 == resultat_ewok1.NombreA()
 
     # SW VII : Le nom des Ewoks 2 !
     # Combien contiennent 2 fois plus de consonnes que de voyelles ?
     def test_SW7_EwokName2(self):
         names = ["Albert", "Barnabé", "Chris"]
-        assert 1 == script.PyDefisSW.SW7_EwokName2(names)
+        resultat_ewok2 = script.SW7EwokName2(names)
+        assert 1 == resultat_ewok2.Double_Voyelle()
