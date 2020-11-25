c03f7fb7cbf42cc87a8e838caada05adbf1a8609
vdf13 <vdf13@free.fr>
2020-11-25 16:33:27 +0100

    Modified pokemon défis

--- pokemondefis.py
+++ pokemondefis.py
@@ -5,32 +5,50 @@
 
 import requests
 
-r = requests.get('https://pydefis.callicode.fr/defis/PokePlusRare/input')
-if r.status_code == 200:
-    print('Success ! ')
-elif r.status_code == 404:
-    print('Not Found. ')
-print(r.status_code)
 
-# récupérer la partie texte de la page web
-contenu = r.text.splitlines()
+class Pokemon:
+    # Défis Pokemon c0d1ngUP 2017
 
-# nettoyage enlève les marques html  </body> <pre>....
-liste_propre = []
-for mot in contenu:
-    if "<" not in mot:
-        liste_propre.append(mot)
+    def __init__(self, fichier):
+        self.fichier = fichier
+        self.result = ""
+        self.ouverture()
+        self.nettoyage()
 
-poke = {}
-# faire une boucle en ne prenant que le nom en key et position en valeurs
-for mot in liste_propre:
-    if ' ' not in mot:
-        cc = mot.split(",")
-        if cc[0] in poke.keys():
-            poke[cc[0]] = (cc[1], cc[2], "multiple")
-        else:
-            poke[cc[0]] = (cc[1], cc[2])
+    def ouverture(self):
+        r = requests.get(self.fichier)
+        if r.status_code == 200:
+            print('Success ! ')
+        elif r.status_code == 404:
+            print('Not Found. ')
+        print(r.status_code)
+        # récupérer la partie texte de la page web
+        self.contenu = r.text.splitlines()
+        return self.contenu
 
-for key, value in poke.items():
-    if "multiple" not in value:
-        print("voici le pokemon unique :", key, value)
+    def nettoyage(self):
+        # nettoyage enlève les marques html  </body> <pre>....
+        liste_propre = []
+        for mot in self.contenu:
+            if "<" not in mot:
+                liste_propre.append(mot)
+        poke = {}
+        # faire une boucle en ne prenant que
+        # le nom en key et position en valeurs
+        for mot in liste_propre:
+            if ' ' not in mot:
+                cc = mot.split(",")
+                cc.append(0)
+                if cc[0] in poke.keys():
+                    poke[cc[0]] = (cc[1], cc[2], cc[3] + 1)
+                else:
+                    poke[cc[0]] = (cc[1], cc[2], cc[3])
+        for key, value in poke.items():
+            if 0 in value:
+                self.result = key, value
+        return self.result
+
+
+fichier = 'https://pydefis.callicode.fr/defis/PokePlusRare/input'
+poke_rare = Pokemon(fichier)
+print(poke_rare.result)
--- test_pokemondefis.py
+++ test_pokemondefis.py
@@ -1,3 +1,14 @@
 #!/bin/usr/python
 
+import pokemondefis as script
 
+# Définition de la classe de test du fichier pokemondefis.py
+
+
+class Test_Pokemon:
+    # Pokemon le plus rare
+    def test_PokeRare(self):
+        # On crée une instance de l'objet
+        fichier = 'https://pydefis.callicode.fr/defis/PokePlusRare/input'
+        resultat = script.Pokemon(fichier)
+        assert ("aflamanoir", ('47', '-115', 0)) == resultat.result
